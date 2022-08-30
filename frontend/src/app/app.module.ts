3c80d4e796691b3f8bb4f1d56667303b9487bbbd
Vasil Evgeniev Aleksandrov <vvvaleksandrovv@gmail.com>
2022-08-30 14:51:53 +0300

    changed to front end folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCard } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCard
   ],
   imports: [
     BrowserModule,
